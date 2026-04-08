8e6a9922550ab0329bca1a4540511723d8d05f9c
Shobhit Narain <shobhit_narain@hotmail.com>
2026-04-08 12:43:17 -0700

    Add: meeting-schedule-ii - submission-9

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-9.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        map<int, int> timeline;
+        for (const auto& interval: intervals) {
+            timeline[interval.start]++;
+            timeline[interval.end]--;
+        }
+        int res = 0, rooms = 0;
+        for (const auto& [t, ev] : timeline) {
+            rooms += ev;
+            res = max(res, rooms);
+        }
+        return res;
+    }
+};
