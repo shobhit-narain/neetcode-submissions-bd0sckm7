1074a8d44ddaff782266613dbf1b1b52910fa4f2
Shobhit Narain <shobhit_narain@hotmail.com>
2026-04-08 12:25:30 -0700

    Add: search-for-word - submission-3

---
+++ Data Structures & Algorithms/search-for-word/submission-3.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int rows, cols;
+
+    bool exist(vector<vector<char>>& board, string word) {
+        rows = board.size();
+        cols = board[0].size();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j<cols; j++) {
+                if(dfs(board, word, 0, i, j)) return true;
+            }
+        }
+        return false;
+    }
+
+    bool dfs(vector<vector<char>>& board, string word, int idx, int x, int y) {
+        if (idx == word.size()) return true;
+        if (x < 0 || y < 0 || x >= rows || y >=cols || 
+            board[x][y] != word[idx] || board[x][y] == '-1') {
+                return false;
+            }
+        board[x][y] = '-1';
+        bool res = dfs(board, word, idx+1, x + 1, y) ||
+                dfs(board, word, idx+1, x, y + 1) ||
+                dfs(board, word, idx+1, x - 1, y) ||
+                dfs(board, word, idx+1, x, y - 1);
+        board[x][y] = word[idx];
+        return res;
+    }
+};
